2a5a6705a07ffce9a0d96809db838fa7d204d44f
Aibek Kendirbaev <kendirbaev.aibek@gmail.com>
2017-01-18 13:08:00 +0600

    added sovet2

---
+++ sovet2/js/main.js
@@ -0,0 +1,64 @@
+jQuery(document).ready(function($){
+
+    //if you change this breakpoint in the style.css file (or _layout.scss if you use SASS), don't forget to update this value as well
+    var $L = 1200,
+        $menu_navigation = $('#main-nav'),
+        $hamburger_icon = $('#hamburger-menu'),
+        $shadow_layer = $('#shadow-layer');
+
+    //open lateral menu on mobile
+    $hamburger_icon.on('click', function(event){
+        event.preventDefault();
+        toggle_panel_visibility($menu_navigation, $shadow_layer, $('body'));
+    });
+
+    //close lateral cart or lateral menu
+    $shadow_layer.on('click', function(){
+        $shadow_layer.removeClass('is-visible');
+        $menu_navigation.removeClass('speed-in').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+            $('body').removeClass('overflow-hidden');
+        });
+    });
+
+    //move #main-navigation inside header on laptop
+    //insert #main-navigation after header on mobile
+    move_navigation($menu_navigation, $L);
+    console.log('test');
+    $(window).on('resize', function(){
+        move_navigation( $menu_navigation, $L);
+
+        if( $(window).width() >= $L && $menu_navigation.hasClass('speed-in')) {
+            $menu_navigation.removeClass('speed-in');
+            $shadow_layer.removeClass('is-visible');
+            $('body').removeClass('overflow-hidden');
+        }
+
+    });
+
+});
+
+function toggle_panel_visibility ($lateral_panel, $background_layer, $body) {
+    if( $lateral_panel.hasClass('speed-in') ) {
+        // firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+        $lateral_panel.removeClass('speed-in').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+            $body.removeClass('overflow-hidden');
+        });
+        $background_layer.removeClass('is-visible');
+
+    } else {
+        $lateral_panel.addClass('speed-in').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+            $body.addClass('overflow-hidden');
+        });
+        $background_layer.addClass('is-visible');
+    }
+}
+
+function move_navigation( $navigation, $MQ) {
+    if ( $(window).width() >= $MQ ) {
+        $navigation.detach();
+        $navigation.appendTo('header');
+    } else {
+        $navigation.detach();
+        $navigation.insertAfter('header');
+    }
+}
