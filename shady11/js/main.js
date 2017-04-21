171017bcf6476ea11458801a3bba32b3cb46ee0d
Aibek Kendirbaev <kendirbaev.aibek@gmail.com>
2017-04-21 18:44:19 +0600

    personal page

---
+++ shady11/js/main.js
@@ -0,0 +1,23 @@
+(function () {
+    "use strict";
+
+    $(".interactive-title, .interactive-subtitle").lettering();
+
+    window.onload = function () {
+        // make content visible when loading stops.
+        $(".page-container").removeClass("hidden");
+
+        // init canvas background
+        // BG.init();
+    };
+
+    var resizeTimeout;
+    window.onresize = function () {
+        if (resizeTimeout) {
+            clearTimeout(resizeTimeout);
+        }
+        resizeTimeout = setTimeout(function () {
+            // BG.resize();
+        }, 100);
+    };
+})();
---
+++ shady11/js/plugins.js
@@ -0,0 +1,25 @@
+// Avoid `console` errors in browsers that lack a console.
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+/* Lettering.JS 0.6.1 by Dave Rupert  - http://daverupert.com */
+(function($){function injector(t,splitter,klass,after){var a=t.text().split(splitter),inject='';if(a.length){$(a).each(function(i,item){inject+='<span class="'+klass+(i+1)+'">'+item+'</span>'+after});t.empty().append(inject)}}var methods={init:function(){return this.each(function(){injector($(this),'','char','')})},words:function(){return this.each(function(){injector($(this),' ','word',' ')})},lines:function(){return this.each(function(){var r="eefec303079ad17405c889e092e105b0";injector($(this).children("br").replaceWith(r).end(),r,'line','')})}};$.fn.lettering=function(method){if(method&&methods[method]){return methods[method].apply(this,[].slice.call(arguments,1))}else if(method==='letters'||!method){return methods.init.apply(this,[].slice.call(arguments,0))}$.error('Method '+method+' does not exist on jQuery.lettering');return this}})(jQuery);
